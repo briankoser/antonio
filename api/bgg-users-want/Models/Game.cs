4ce142e6934acf8b465c1136e2782e8d38d5238e
Brian Koser <brianmkoser@gmail.com>
2015-05-18 22:50:37 -0500

    Add models

---
+++ api/bgg-users-want/Models/Game.cs
@@ -0,0 +1,58 @@
+﻿using Fizzler.Systems.HtmlAgilityPack;
+using HtmlAgilityPack;
+using HtmlDoc = HtmlAgilityPack.HtmlDocument;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace bgg_users_want.Models
+{
+    public class Game
+    {
+        public int ID { get; private set; }
+        public List<User> UsersWant { get; set; }
+
+        public Game(int id)
+        {
+            ID = id;
+            UsersWant = GetUsersWantList(id);
+        }
+
+        public Game(int id, List<User> usersWant)
+        {
+            ID = id;
+            UsersWant = usersWant;
+        }
+
+        private List<User> GetUsersWantList(int id)
+        {
+            var htmlWeb = new HtmlWeb();
+            int page = 1;
+            bool usersExist = true;
+            var users = new List<User>();
+
+            while (usersExist)
+            {
+                usersExist = false;
+
+                string url = "http://boardgamegeek.com/collection/items/boardgame/" + id + "/page/" + page + "?want=1";
+                var html = htmlWeb.Load(url);
+                var divs = html.DocumentNode.QuerySelectorAll(".avatarblock[data-username]");
+
+                foreach (var div in divs)
+                {
+                    users.Add(new User(div.GetAttributeValue("data-username", String.Empty)));
+                    usersExist = true;
+                }
+
+                page += 1;
+            }
+
+            users.RemoveAll(x => x.Name == String.Empty);
+
+            return users;
+        }
+    }
+}
---
+++ api/bgg-users-want/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace bgg_users_want.Models
+{
+    public class User
+    {
+        public string Name { get; private set; }
+
+        public User(string name)
+        {
+            Name = name;
+        }
+    }
+}

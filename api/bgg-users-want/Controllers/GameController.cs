02432d7d68d082fbf0d19acfc672341034368473
Brian Koser <brianmkoser@gmail.com>
2015-05-18 22:51:50 -0500

    Add controller

---
+++ api/bgg-users-want/Controllers/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using bgg_users_want.Models;
+
+namespace bgg_users_want.Controllers
+{
+    public class GameController : ApiController
+    {
+        public IHttpActionResult GetGame(int id)
+        {
+            var game = new Game(id);
+
+            if (game == null)
+            {
+                return NotFound();
+            }
+            return Ok(game);
+        }
+    }
+}

8f94c2dc2601f8a680ee75831525e0e0d04cf778
Brian Koser <brianmkoser@gmail.com>
2015-05-30 16:26:52 -0500

    Sleep in between calls to BGG

--- api/bgg-users-want/Models/Game.cs
+++ api/bgg-users-want/Models/Game.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Threading;
 
 namespace bgg_users_want.Models
 {
@@ -48,6 +49,7 @@ namespace bgg_users_want.Models
                 }
 
                 page += 1;
+
             }
 
             users.RemoveAll(x => x.Name == String.Empty);
